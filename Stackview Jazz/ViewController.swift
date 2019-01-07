c899ef2dba245322e057103ce545e272b36a7672
Steve Lipton <stevelipton@brk-16892.local>
2019-01-07 06:09:52 -0600

    Initial Commit

---
+++ Stackview Jazz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stackview Jazz
+//
+//  Created by Steven Lipton on 12/10/18.
+//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
